00b400720b1256b84b1039720312da623b0dd9bf
tanmaywithin <tanmaykhandelwal962@gmail.com>
2025-09-01 11:11:25 +0530

    Remove env file from repo

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname,'public')));
 app.use(cookieParser());
 
-require("dotenv").config();
-mongoose.connect(process.env.MONGO_URI);
+mongoose.connect(process.env.MONGO_URI)
+
 
 
 app.get("/",function(req,res){
